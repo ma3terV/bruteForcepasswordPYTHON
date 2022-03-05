b0dafbd738240caf1af8211d3e380d3d7be5ef29
Vasu Bansal <96645222+ma3terV@users.noreply.github.com>
2022-03-05 10:42:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,14 +9,14 @@ chars = "abcdefghijklmnopqrstuvwxyz0123456789"
 chars_list = list(chars)
 
 
-password = pyautogui.password("Enter a password : ")
+password = pyautogui.password("Enter password : ")
 
 guess_password = ""
 
 while(guess_password != password):
     guess_password = random.choices(chars_list, k=len(password))
 
-    print("<=================="+ str(guess_password)+ "==================>")
+    print("Guessing password: <===="+ str(guess_password)+ "====>")
 
     if(guess_password == list(password)):
         print("Your password is : "+ "".join(guess_password))
